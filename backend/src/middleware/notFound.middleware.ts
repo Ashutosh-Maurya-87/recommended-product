d106586602259dfd9d1d87923931a76153801c77
Ashutosh Maurya <ashumaurya486@gmail.com>
2026-08-13 07:32:14 +0530

    create not found middleware

---
+++ backend/src/middleware/notFound.middleware.ts
@@ -0,0 +1,16 @@
+import {
+  Request,
+  Response,
+  NextFunction,
+} from "express";
+
+export function notFoundHandler(
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  res.status(404).json({
+    success: false,
+    message: `Route ${req.method} ${req.originalUrl} not found`,
+  });
+}
